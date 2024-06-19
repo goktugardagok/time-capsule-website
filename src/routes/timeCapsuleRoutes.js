6996232bdd9af72cad982fdcd135c18403660b90
goktuggok <goktugardagok0987@gmail.com>
2024-06-19 03:21:21 +0300

    Fix route and controller issues

--- src/routes/timeCapsuleRoutes.js
+++ src/routes/timeCapsuleRoutes.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const timeCapsuleController = require('../controllers/timeCapsuleController');
-const multer = require('multer');
 
-const upload = multer({ dest: 'uploads/' });
-
-router.get('/', (req, res) => {
-  res.send('Time Capsules API');
-});
-
-router.post('/create', upload.single('file'), timeCapsuleController.createTimeCapsule);
+// GET /api/timecapsules/all
 router.get('/all', timeCapsuleController.getAllTimeCapsules);
 
 module.exports = router;
