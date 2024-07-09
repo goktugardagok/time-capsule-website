786df3d95ee4aa4adb9df657fb1fc2a196f66e04
goktuggok <goktugardagok0987@gmail.com>
2024-07-09 17:04:20 +0300

    Configure MongoDB with GridFS

--- src/utils/fileStorage.js
+++ src/utils/fileStorage.js
@@ -1,15 +1,17 @@
-const GridFsStorage = require('multer-gridfs-storage');
+const GridFsStorage = require('multer-gridfs-storage').GridFsStorage;
 const multer = require('multer');
+require('dotenv').config();
 
-// MongoDB URI
+// MongoDB URI from environment variables
 const mongoURI = process.env.MONGODB_URI;
 
 // Create storage engine
 const storage = new GridFsStorage({
     url: mongoURI,
+    options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
         return {
-            bucketName: 'uploads', // Bucket name in MongoDB
+            bucketName: 'uploads', // bucket name in MongoDB
             filename: `${Date.now()}-${file.originalname}`
         };
     }
