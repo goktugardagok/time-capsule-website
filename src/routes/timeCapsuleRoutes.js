0eff2426445ec94592ef153f951f0b77f982e21f
goktuggok <goktugardagok0987@gmail.com>
2024-07-10 16:15:53 +0300

    Add multer-gridfs-storage and configure MongoDB for file storage

--- src/routes/timeCapsuleRoutes.js
+++ src/routes/timeCapsuleRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const timeCapsuleController = require('../controllers/timeCapsuleController');
 
-router.post('/create', timeCapsuleController.createTimeCapsule);
+router.post('/submit', (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
+    res.status(201).send('Data received successfully');
+});
 
 module.exports = router;
