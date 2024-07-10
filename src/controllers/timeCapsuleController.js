6f289defb823264f18dd50200b7bffacc5518efc
goktuggok <goktugardagok0987@gmail.com>
2024-07-10 15:50:40 +0300

    Add multer-gridfs-storage and configure MongoDB for file storage999

--- src/controllers/timeCapsuleController.js
+++ src/controllers/timeCapsuleController.js
@@ -1,31 +1,5 @@
 const TimeCapsule = require('../models/timeCapsuleModel');
-const mongoose = require('mongoose');
-const GridFsStorage = require('multer-gridfs-storage');
-const multer = require('multer');
-const crypto = require('crypto');
-const path = require('path');
-const Grid = require('gridfs-stream');
-
-// Create a storage object with a given configuration
-const storage = new GridFsStorage({
-  url: process.env.MONGODB_URI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileInfo);
-      });
-    });
-  }
-});
-const upload = multer({ storage });
+const upload = require('../utils/fileStorage');
 
 const createTimeCapsule = async (req, res) => {
   try {
