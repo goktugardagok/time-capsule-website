8727f93d7ef3a421ae72309daf78eb52ffad37e7
goktuggok <goktugardagok0987@gmail.com>
2024-07-12 15:20:07 +0300

    Add views directory and index.ejs

--- src/app.js
+++ src/app.js
@@ -1,32 +1,39 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const timeCapsuleRoutes = require('./routes/timeCapsuleRoutes'); // Correct path
+const path = require('path');
+const timeCapsuleRoutes = require('./routes/timeCapsuleRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Set EJS as templating engine and set views directory
+// Set the view engine to ejs
 app.set('view engine', 'ejs');
-app.set('views', './src/views'); // Correct path
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
 app.use('/api', timeCapsuleRoutes);
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.log(err));
-
-// Route to display data
-app.get('/', async (req, res) => {
-    const TimeCapsule = mongoose.model('TimeCapsule');
-    const data = await TimeCapsule.find();
-    res.render('index', { data });
+// Default route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Time Capsule API');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
