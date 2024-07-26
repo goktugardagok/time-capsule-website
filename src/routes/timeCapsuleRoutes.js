2a014063129c248aacbc8688138f594c093ab355
goktuggok <goktugardagok0987@gmail.com>
2024-07-27 00:58:52 +0300

    Added missing dependencies and updated configurations....

--- src/routes/timeCapsuleRoutes.js
+++ src/routes/timeCapsuleRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const timeCapsuleController = require('../controllers/timeCapsuleController');
 
-router.post('/submit', timeCapsuleController.createTimeCapsule);
+router.post('/create', timeCapsuleController.createTimeCapsule);
 router.get('/countdown/:id', timeCapsuleController.getCountdown);
 router.get('/content/:id', timeCapsuleController.getContent);
 
