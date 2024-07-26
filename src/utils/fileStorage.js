ce28d623c9d922f0dcc6176374b84ff2cf55dd63
goktuggok <goktugardagok0987@gmail.com>
2024-07-27 00:25:30 +0300

    Added missing dependencies and updated configurations....ooo

--- src/utils/fileStorage.js
+++ src/utils/fileStorage.js
@@ -1,14 +1,25 @@
 const { GridFsStorage } = require('multer-gridfs-storage');
 const multer = require('multer');
+const crypto = require('crypto');
+const path = require('path');
 
 const storage = new GridFsStorage({
     url: process.env.MONGODB_URI,
     options: { useNewUrlParser: true, useUnifiedTopology: true },
     file: (req, file) => {
-        return {
-            bucketName: 'uploads', // Setting the collection name, default name is fs
-            filename: `${Date.now()}-${file.originalname}`
-        };
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                const fileInfo = {
+                    filename: filename,
+                    bucketName: 'uploads'
+                };
+                resolve(fileInfo);
+            });
+        });
     }
 });
 
