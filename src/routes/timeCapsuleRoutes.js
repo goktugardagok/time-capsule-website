533fc5853178c9aa7cb6c7bbda53bdb984ecb387
goktuggok <goktugardagok0987@gmail.com>
2024-07-26 22:56:42 +0300

    Added missing dependencies and updated configurations

--- src/routes/timeCapsuleRoutes.js
+++ src/routes/timeCapsuleRoutes.js
@@ -4,7 +4,7 @@ const timeCapsuleController = require('../controllers/timeCapsuleController');
 const upload = require('../utils/fileStorage');
 
 router.post('/submit', upload.single('file'), timeCapsuleController.createTimeCapsule);
-router.get('/:id/countdown', timeCapsuleController.getCountdown);
-router.get('/:id/content', timeCapsuleController.getContent);
+router.get('/countdown/:id', timeCapsuleController.getCountdown);
+router.get('/content/:id', timeCapsuleController.getContent);
 
 module.exports = router;
