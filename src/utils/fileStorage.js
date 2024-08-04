8f443cacf0083fd2ea9bb99fe61328328bca9049
goktuggok <goktugardagok0987@gmail.com>
2024-08-04 20:44:29 +0300

    Describe your changes here

--- src/utils/fileStorage.js
+++ src/utils/fileStorage.js
@@ -1,44 +1,21 @@
-// src/utils/fileStorage.js
-const multer = require('multer');
 const { GridFsStorage } = require('multer-gridfs-storage');
 const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Mongo URI from .env
 const mongoURI = process.env.MONGODB_URI;
 
-// Create MongoDB connection
-const conn = mongoose.createConnection(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-let gfs;
-
-conn.once('open', () => {
-    // Init stream
-    gfs = Grid(conn.db, mongoose.mongo);
-    gfs.collection('uploads');
-});
-
-// Create storage engine
 const storage = new GridFsStorage({
     url: mongoURI,
     file: (req, file) => {
-        return new Promise((resolve, reject) => {
-            const filename = Date.now() + path.extname(file.originalname);
-            const fileInfo = {
-                filename: filename,
-                bucketName: 'uploads'
-            };
-            resolve(fileInfo);
-        });
+        return {
+            filename: Date.now() + path.extname(file.originalname),
+            bucketName: 'uploads'
+        };
     }
 });
 
 const upload = multer({ storage });
 
-module.exports = { upload, gfs };
+module.exports = upload;
